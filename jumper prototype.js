d4bbd385d9721d5f880aed52d805b6ab30f9780a
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2017-12-29 00:39:47 +0800

    Prototype

---
+++ jumper prototype.js
@@ -0,0 +1,33 @@
+// Damai Script for Aqours FMT
+
+let num = '1';
+let proId = test139316.front;
+
+// Do NOT edit the following
+
+let test139316 = {
+    stu: '12084693',
+    back: '12084692',
+    front: '12084798'
+}
+
+let aqours = {
+    day1280: '12013117',
+    day1080: '12013118',
+    day880a: '12068613',
+    day880ext: '12089426',
+    day880: '12068621',
+    day680: '12013119',
+    day480: '12068614',
+    night1280: '12068634',
+    night1080: '12068635',
+    night880ext: '12089425',
+    night880: '12068639',
+    night880a: '12068637',
+    night680: '12068636',
+    night480: '12068638'
+}
+
+let companyId = '872'; // 872 for Shanghai
+
+location.href = "//www.damai.cn/GotoShopping.aspx?_action=Immediately&proId=" + proId + "&optype=1&companyId=" + companyId + "&num=" + num + "&n=0"
